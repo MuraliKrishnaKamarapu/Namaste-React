f80f112e267a9d777ee261766d74d62c9504a5f8
Murali Krishna Kamarapu <91290217+MuraliKrishnaKamarapu@users.noreply.github.com>
2024-01-02 18:55:00 -0500

    first

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {}, "This is from React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const parent = React.createElement("div", {},);
+
+root.render(heading);
